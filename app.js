db4d6123ca3c15aaca8c2dc791eaf7ce71216622
Steven O <steveno@liatrio.com>
2022-01-26 11:17:48 -0600

    fixed environment variables and import

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const { App, createNodeMiddleware } = require("@octokit/app");
 require('dotenv').config()
-import express from "express";
+
+const express = require("express");
 
 const expressApp = express();
 
 const app = new App({
-    appId: process.env.APP_ID,
-    privateKey: process.env.PRIVATE_KEY,
+    appId: process.env.GITHUB_APP_ID,
+    privateKey: process.env.GITHUB_APP_PRIVATE_KEY,
     oauth: {
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
+        clientId: process.env.GITHUB_APP_CLIENT_ID,
+        clientSecret: process.env.GITHUB_APP_CLIENT_SECRET,
     },
     webhooks: {
-        secret: "secret",
+        secret: process.env.GITHUB_SECRET,
     },
 });
 
@@ -48,13 +49,17 @@ app.webhooks.on("repository.created", async ({ octokit, payload }) => {
 expressApp.use(createNodeMiddleware(app));
 
 expressApp.get('/', (req, res) => {
+    console.log(`Healthcheck on ${req.path}`)
     res.send('ok')
 })
 
 expressApp.get('/healthcheck', (req, res) => {
+    console.log(`Healthcheck on ${req.path}`)
     res.send('ok')
 })
 
 const port = process.env.PORT || 3000;
+
+console.log(process.env)
 
 expressApp.listen(port, '0.0.0.0');
