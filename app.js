b397dc1cf6283e94d94640d9a32af30fbae298d9
Calvin Wai <cwai@MacBook-Pro.local>
2022-01-27 10:20:29 -0800

    feat: add branch protection events

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { App, createNodeMiddleware } = require("@octokit/app");
 require('dotenv').config()
 
 const express = require("express");
+const { isObject } = require("util");
 
 const expressApp = express();
 
@@ -12,6 +13,9 @@ const app = new App({
         clientId: process.env.GITHUB_APP_CLIENT_ID,
         clientSecret: process.env.GITHUB_APP_CLIENT_SECRET,
     },
+    // webhooks: {
+    //     secret: "secret",
+    // },
     webhooks: {
         secret: process.env.WEBHOOK_SECRET,
     },
@@ -25,33 +29,35 @@ const org_events = [
 ]
 
 app.webhooks.on(org_events, async ({ octokit, payload }) => {
-
     // console.log(payload);
     // console.log(`Configuring branch protection for repo ${payload.repository.name}`)
-    await octokit.request(
-        "PUT /repos/{owner}/{repo}/branches/{branch}/protection",
-        {
-            owner: payload.repository.owner.login,
-            repo: payload.repository.name,
-            branch: payload.repository.default_branch,
-            required_status_checks: {
-                contexts: [],
-                strict: true,
-            },
-            enforce_admins: true,
-            required_pull_request_reviews: {
-                dismiss_stale_reviews: true,
-                required_approving_review_count: 1,
-            },
-            required_linear_history: true,
-            allow_force_pushes: false,
-            allow_deletions: false,
-            required_conversation_resolution: true,
-            restrictions: null,
-        }
-    );
-
+    if(payload.sender.type !== 'Bot') {
+        await octokit.request(
+            "PUT /repos/{owner}/{repo}/branches/{branch}/protection",
+            {
+                owner: payload.repository.owner.login,
+                repo: payload.repository.name,
+                branch: "main",
+                required_status_checks: {
+                    contexts: [],
+                    strict: true,
+                },
+                enforce_admins: true,
+                required_pull_request_reviews: {
+                    dismiss_stale_reviews: true,
+                    required_approving_review_count: 1,
+                },
+                required_linear_history: true,
+                allow_force_pushes: false,
+                allow_deletions: false,
+                required_conversation_resolution: true,
+                restrictions: null,
+            }
+        );
+    }
 });
+
+require("http").createServer(createNodeMiddleware(app)).listen(3001);
 
 expressApp.use(createNodeMiddleware(app));
 
