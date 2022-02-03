5f5e4803e1ffe9a7c6c8739d2b6f8c0185751eb8
wai-calvin <wai.calvin96@gmail.com>
2022-02-03 09:49:26 -0800

    revert repo logic to use period delimiter and repo update (#13)
    * revert repo logic to use period delimiter and repo update
    
    * add error handling
    
    Co-authored-by: Calvin Wai <cwai@MacBook-Pro.local>

--- handlers/repo_team_manager.js
+++ handlers/repo_team_manager.js
@@ -1,27 +1,38 @@
-module.exports = async function({ octokit, payload }) {
+module.exports = async function ({ octokit, payload }) {
     const repo_name = payload.repository.name;
+    if (!repo_name.includes(".")) {
+        console.log("No team name found in repo name.");
+        return;
+    }
 
-    const teams = await octokit.request(
-        "GET /orgs/{org}/teams",
+    const repo_name_parse = repo_name.split('.');
+    const team_slug = repo_name_parse[0];
+
+    const team_response = await octokit.request(
+        "GET /orgs/{org}/teams/{team_slug}",
         {
-            org: payload.repository.owner.login
+            org: payload.repository.owner.login,
+            team_slug: team_slug
         }
-    )
-
-    teams.data.map(async team => {
-        if(!repo_name.startsWith(team.slug)) {
-            return;
+    ).catch(error => {
+        console.log(error);
+    });
+    
+    if (team_response?.status !== 200) {
+        console.log("Team response: " + team_response?.status);
+        return;
+    };
+    
+    await octokit.request(
+        "PUT /orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}",
+        {
+            org: payload.repository.owner.login,
+            team_slug: team_response.data.slug,
+            owner: payload.repository.owner.login,
+            permission: "maintain",
+            repo: repo_name
         }
-
-        await octokit.request(
-            "PUT /orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}",
-            {
-                org: payload.repository.owner.login,
-                team_slug: team.slug,
-                owner: payload.repository.owner.login,
-                permission: "maintain",
-                repo: repo_name
-            }
-        )
+    ).catch(error => {
+        console.log(error);
     });
 }
